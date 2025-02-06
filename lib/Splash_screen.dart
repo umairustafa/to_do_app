851e4193f96d7dd437cb71e13a685a0ebe0b72d9
umairustafa <umairmustafa693@gmail.com>
2025-02-06 13:04:55 +0500

    message

---
+++ lib/Splash_screen.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:to_do_app/view/auth/login_screen.dart';
+
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    
+                    
+    super.initState();
+    Timer(Duration(seconds: 4) ,(){
+            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+         
+          height: double.infinity,
+        width: double.infinity,
+    
+        decoration: BoxDecoration(
+         color: const Color.fromARGB(255, 163, 104, 104)
+        ),
+         
+        ),
+    );
+  }
+}
