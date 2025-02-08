8db3b5930ee41c696df90b5336742a0cfb539819
umairustafa <umairmustafa693@gmail.com>
2025-02-08 12:03:08 +0500

    message

---
+++ lib/constant/app_icons.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  static const icon1=Icon(Icons.home);
+}
---
+++ lib/constant/app_images.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppImages {
+  static const Image1=Image(image: AssetImage(''));
+}
